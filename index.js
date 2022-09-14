a6e9d2570978820c2525dc43d3af84b1edc04571
tamirkash <tamirkash@gmail.com>
2022-09-14 17:54:56 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const { ECRClient, BatchGetImageCommand } = require('@aws-sdk/client-ecr');
+
+try {
+  let digest;
+  const architecture = core.getInput('architecture');
+  const awsRegion = core.getInput('aws-region');
+  const repositoryName = core.getInput('repository-name');
+  const imageTag = core.getInput('image-tag');
+
+  console.log(`Resolving image for architecture: ${architecture}!`);
+  const client = new ECRClient({ region: awsRegion });
+  const input = {
+    imageIds: [
+      {
+        imageTag
+      }
+    ],
+    repositoryName
+  };
+  const command = new BatchGetImageCommand(input);
+  client.send(command).then(response => {
+    if (!response.mediaType || !response.mediaType.includes("manifest")) {
+        throw {
+          message: "could not find any manifest list"
+        };
+      }
+
+      response.manifests.array.forEach(element => {
+        if (element.platform.architecture === architecture) {
+          digest = element.digest;
+        }
+      });
+
+      if (!digest) {
+        throw {
+          message: "could not find an image with the required architecture"
+        };
+      }
+      
+      core.setOutput("digest", digest);
+      console.log(`Setting output.digest: ${digest}`);
+  }).catch(err => {
+    console.log("failed during fetching manifest list. ", err);
+  });
+} catch (error) {
+  core.setFailed(error.message);
+}
